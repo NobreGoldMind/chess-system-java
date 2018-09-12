1c629a704630ba18c285f9ad7f941e3cf32af6cf
NobreGoldMind <mateusdantasnobre@outlook.com>
2018-09-12 11:21:54 -0300

    Primeiro Teste

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,13 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
+
+		Position pos = new Position(5, 3);
+		System.out.println(pos);
 
 	}
 
